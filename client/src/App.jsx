b0eb6a370290cc4da9fbb4cd9a44e400666da7a9
Thiraphan24 <thiraphan.codercamp@gmail.com>
2023-10-03 18:52:15 +0700

    feat: play music

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useRef, useEffect } from "react";
+import { CiPlay1 } from "react-icons/ci";
+import { TbPlayerTrackNext } from "react-icons/tb";
+import { FaFastBackward } from "react-icons/fa";
+const songs = ["Contra", "HavestMoon", "Mario"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(2);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    loadSong(songs[index]);
+  }, [index]);
+
+  const loadSong = (song) => {
+    const audio = audioRef.current;
+    audio.src = `music/${song}.mp3`;
+    audio.load();
+  };
+
+  const playSong = () => {
+    const audio = audioRef.current;
+    audio.play();
+    setIsPlaying(true);
+  };
+
+  const pauseSong = () => {
+    const audio = audioRef.current;
+    audio.pause();
+    setIsPlaying(false);
+  };
+
+  const nextSong = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % songs.length);
+  };
+
+  const prevSong = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gradient-to-r from-blue-500 to-teal-400 h-screen flex flex-col items-center justify-center">
+      <h1 className="text-white mb-8 text-4xl">โปรแกรมเล่นเพลง (Music API)</h1>
+      <div className="music-container bg-white rounded-lg p-6 flex items-center justify-between relative w-[350px]">
+        <div className="img-container w-20 h-20 relative">
+          <img
+            src={`cover/${songs[index]}.jpg`}
+            alt={`Cover for ${songs[index]}`}
+            className="w-full h-full rounded-full object-cover animate-spin"
+          />
+        </div>
+        <div className="music-info absolute top-0 left-20 opacity-0 transform -translate-y-full transition-transform ease-in duration-300 bg-white rounded-lg p-2">
+          <h4 className="title">เพลง {songs[index]}.mp3</h4>
+        </div>
+        <audio ref={audioRef} src={`music/${songs[index]}.mp3`} hidden></audio>
+
+        <div className="navigation flex items-center">
+          <FaFastBackward
+            onClick={prevSong}
+            className="action-btn mr-[20px] ml-[20px]"
+          >
+            <i className="fas fa-backward text-2xl">back</i>
+          </FaFastBackward>
+          <button
+            onClick={isPlaying ? pauseSong : playSong}
+            className="action-btn "
+          >
+            <CiPlay1
+              className={`fas ${isPlaying ? "fa-pause" : "fa-play"} text-2xl`}
+            ></CiPlay1>
+          </button>
+          <button onClick={nextSong} className="action-btn ml-[20px]">
+            <TbPlayerTrackNext className="fas fa-forward text-2xl"></TbPlayerTrackNext>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
